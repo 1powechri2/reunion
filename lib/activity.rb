87d3d7909f50fb046ab5b40ca48b67800cdef60d
Chris Powell <chris.ian.powell@gmail.com>
2018-05-30 09:53:29 -0600

    activity class finished

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,13 +1,22 @@
 class Activity
   attr_reader :name,
+              :base_cost,
+              :cost_per_person,
               :participants
 
-  def initialize(name)
+  def initialize(name, base_cost, cost_per_person)
     @name = name
+    @base_cost = base_cost
+    @cost_per_person = cost_per_person
     @participants = Hash.new
   end
 
   def add_participants(new_participant, amount_paid)
     @participants[new_participant] = amount_paid
+  end
+
+  def evaluate_total_cost
+    total_per_person = @cost_per_person * @participants.keys.length
+    return @base_cost + total_per_person
   end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,20 +2,33 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
   def test_activity_exists
-    activity = Activity.new('Funhouse')
+    activity = Activity.new('Funhouse', 500, 10)
     assert_instance_of Activity, activity
   end
 
   def test_activities_have_names
-    activity = Activity.new('Funhouse')
+    activity = Activity.new('Funhouse', 500, 10)
     assert_equal 'Funhouse', activity.name
   end
 
+  def test_base_cost
+    activity = Activity.new('Funhouse', 500, 10)
+
+    assert_equal 500, activity.base_cost
+  end
+
+  def test_cost_per_person
+    activity = Activity.new('Funhouse', 500, 10)
+
+    assert_equal 10, activity.cost_per_person
+  end
+
   def test_can_take_participants
-    activity = Activity.new('Funhouse')
+    activity = Activity.new('Funhouse', 500, 10)
 
     participant = 'El Dude'
     amount_paid = 500
@@ -26,7 +39,7 @@ class ActivityTest < Minitest::Test
   end
 
   def test_add_multiple_participants
-    activity = Activity.new('Funhouse')
+    activity = Activity.new('Funhouse', 500, 10)
 
     participant = 'El Dude'
     amount_paid = 500
@@ -42,5 +55,21 @@ class ActivityTest < Minitest::Test
     actual   = activity.participants
 
     assert_equal expected, actual
+  end
+
+  def test_evaluate_total_cost
+    activity = Activity.new('Funhouse', 500, 10)
+
+    participant = 'El Dude'
+    amount_paid = 500
+
+    activity.add_participants(participant, amount_paid)
+
+    new_participant = 'Kat'
+    amount_paid     = 505
+
+    activity.add_participants(new_participant, amount_paid)
+
+    assert_equal 520, activity.evaluate_total_cost
   end
 end
