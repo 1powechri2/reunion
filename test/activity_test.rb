0f5d92a0bfcb6ab63d37b2a5d3b465f338c22824
Chris Powell <chris.ian.powell@gmail.com>
2018-05-30 09:09:18 -0600

    activity test and class started

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,13 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_activity_exists
+    activity = Activity.new('Funhouse')
+    assert_instance_of Activity, activity
+  end
+
+  def test_activities_have_names
+    activity = Activity.new('Funhouse')
+    assert_equal 'Funhouse', activity.name
+  end
 end
